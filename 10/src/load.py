6247874bd7cc14b4ac84e459ff27aa6727ffbc3c
sivasaikasmo <sivasai.somisetty@kasmodigital.com>
2025-09-01 22:23:43 -0500

    Update

--- 10/src/load.py
+++ 10/src/load.py
@@ -1,7 +1,19 @@
 def load(df1,df2,df3,df4,engine):
-    df1.to_sql(name='projects',con=engine,if_exists='replace',index=False)
-    df2.to_sql(name='project_technologies',con=engine,if_exists='replace',index=False)
-    df4.to_sql(name='project_team_members',con=engine,if_exists='replace',index=False)
-    df3.to_sql(name='project_milestones',con=engine,if_exists='replace',index=False)
+    connection=engine.connect()
+    transaction=connection.begin()
 
-    return 'Loaded Succesfully'
+    try:
+        df1.to_sql(name='projects',con=engine,if_exists='replace',index=False)
+        df2.to_sql(name='project_technologies',con=engine,if_exists='replace',index=False)
+        df4.to_sql(name='project_team_members',con=engine,if_exists='replace',index=False)
+        df3.to_sql(name='project_milestones',con=engine,if_exists='replace',index=False)
+
+        transaction.commit()
+        return 'Loaded Succesfully'
+    
+    except Exception as e:
+        transaction.rollback()
+        return 'Transaction Failed'
+    
+    finally:
+        connection.close()
--- 11/src/load.py
+++ 11/src/load.py
@@ -1,5 +1,45 @@
-def load(df1,df2,engine):
-    df1.to_sql(name='project_details',con=engine,if_exists='replace',index=False)
-    df2.to_sql(name='project_technologies',con=engine,if_exists='replace',index=False)
+# def load(df1,df2,engine):
+#     df1.to_sql(name='project_details',con=engine,if_exists='replace',index=False)
+#     df2.to_sql(name='project_technologies',con=engine,if_exists='replace',index=False)
 
-    return 'Loaded Succesfully'
+#     return 'Loaded Succesfully'
+
+from sqlalchemy import Table, Column,ForeignKey, MetaData, Text, DateTime,String
+
+def load(df1,df2,engine):
+    metadata = MetaData()
+
+    project_details = Table('project_details', metadata,
+    Column('project_id', String(50), primary_key=True),
+    Column('project_name', Text()),
+    Column('client', Text()),
+    Column('domain', Text()),
+    Column('location', Text()),
+    Column('project_manager', Text()),
+    Column('start_date', DateTime()),
+    Column('end_date', DateTime()),
+    Column('status', Text()),
+    )
+
+    project_technologies = Table('project_technologies', metadata,
+    Column('project_id', String(50), ForeignKey('project_details.project_id')),
+    Column('technologies', Text()))
+
+    metadata.create_all(engine)
+    
+    connection=engine.connect()
+    transaction=connection.begin()
+
+    try:
+        df1.to_sql(name='project_details',con=engine,if_exists='append',index=False)
+        df2.to_sql(name='project_technologies',con=engine,if_exists='append',index=False)
+
+        transaction.commit()
+        return 'Loaded Succesfully'
+    
+    except Exception as e:
+        transaction.rollback()
+        return 'Transaction Failed'
+    
+    finally:
+        connection.close()
